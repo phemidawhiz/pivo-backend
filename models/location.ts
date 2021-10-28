088e256afd5b17d68d9f5093dd7691baab7f8b0c
phemidawhiz <feminiscence@gmail.com>
2021-10-28 16:27:01 +0100

    add location routes and models

---
+++ models/location.ts
@@ -0,0 +1,70 @@
+import {Location} from "../types/location";
+import {db} from "../db";
+import { OkPacket, RowDataPacket } from "mysql2";
+
+export const create = (location: Location, callback: Function) => { // create location
+    const queryString = "INSERT INTO locations (truck_id, longitude, latitude) VALUES (?, ?, ?)"
+
+    db.query(
+        queryString,
+        [location.truckId, location.longitude, location.latitude],
+        (err, result) => {
+            if (err) {callback(err)};
+
+            const insertId = (<OkPacket> result).insertId;
+            callback(null, insertId);
+        }
+    );
+};
+
+export const findOne = (locationId: number, callback: Function) => { // get a location with location Id
+
+    const queryString = `
+    SELECT 
+      *
+    FROM locations 
+    WHERE id=?`
+
+    db.query(queryString, locationId, (err, result) => {
+        if (err) {callback(err)}
+
+        const row = (<RowDataPacket> result)[0];
+        const location: Location =  {
+            id: row.id,
+            truckId: row.truck_id,
+            longitude: row.longitude,
+            latitude: row.latitude,
+            datetime: row.datetime
+        }
+        callback(null, location);
+    });
+}
+
+export const findAll = (truckId: number, callback: Function) => { // get all locations of a particular truck id
+    const queryString = `
+    SELECT 
+      *
+    FROM 
+    locations
+    WHERE truck_id=?
+    `
+
+    db.query(queryString, truckId, (err, result) => {
+        if (err) {callback(err)}
+
+        const rows = <RowDataPacket[]> result;
+        const locations: Location[] = [];
+
+        rows.forEach(row => {
+            const location: Location =  {
+                id: row.id,
+                truckId: row.truck_id,
+                longitude: row.longitude,
+                latitude: row.latitude,
+                datetime: row.datetime
+            }
+            locations.push(location);
+        });
+        callback(null, locations);
+    });
+}
---
+++ routes/location.ts
@@ -0,0 +1,38 @@
+import express, {Request, Response} from "express";
+import * as locationModel from "../models/location";
+import { Location } from "../types/location";
+const locationRouter = express.Router();
+
+locationRouter.get("/location/:id", async (req: Request, res: Response) => {
+    const truckId: number = Number(req.params.id);
+    locationModel.findAll(truckId, (err: Error, orders: Location[]) => {
+        if (err) {
+            return res.status(500).json({"errorMessage": err.message});
+        }
+        res.status(200).json({"data": orders});
+    });
+});
+
+locationRouter.post("/location/", async (req: Request, res: Response) => {
+    const newLocation: Location = req.body;
+    locationModel.create(newLocation, (err: Error, locationId: number) => {
+        if (err) {
+            return res.status(500).json({"message": err.message});
+        }
+
+        res.status(200).json({"locationId": locationId});
+    });
+});
+
+locationRouter.get("/:id", async (req: Request, res: Response) => {
+    const locationId: number = Number(req.params.id);
+    locationModel.findOne(locationId, (err: Error, order: Location) => {
+        if (err) {
+            return res.status(500).json({"message": err.message});
+        }
+        res.status(200).json({"data": order});
+    })
+});
+
+
+export {locationRouter};
