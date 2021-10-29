de520d9646c045a39628bee6e8c855053cca3805
phemidawhiz <feminiscence@gmail.com>
2021-10-29 15:35:05 +0100

    update sample sql file

--- nodetest.sql
+++ nodetest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2021 at 01:36 AM
+-- Generation Time: Oct 29, 2021 at 04:34 PM
 -- Server version: 10.1.39-MariaDB
 -- PHP Version: 7.2.18
 
@@ -45,7 +45,12 @@ INSERT INTO `trucklocations` (`location_id`, `truck_id`, `longitude`, `latitude`
 (2, 1, '8.3288493', '8.437282', '2020-10-20 13:01:45'),
 (3, 1, '6.764743', '7.647383', '2021-10-28 16:47:36'),
 (4, 2, '6.363743', '7.4383399', '2021-10-28 16:48:59'),
-(5, 3, '6.748383', '7.658586', '2021-10-28 23:05:45');
+(5, 3, '6.748383', '7.658586', '2021-10-28 23:05:45'),
+(6, 3, '6.748383', '7.658586', '2021-10-29 11:07:02'),
+(7, 19, '6.84578', '7.89643', '2021-10-29 14:27:50'),
+(8, 18, '6.84578', '7.89643', '2021-10-29 15:20:29'),
+(9, 24, '6.84578', '7.89643', '2021-10-29 15:20:51'),
+(10, 22, '6.84578', '7.89643', '2021-10-29 15:26:25');
 
 -- --------------------------------------------------------
 
@@ -66,24 +71,28 @@ CREATE TABLE `trucks` (
 --
 
 INSERT INTO `trucks` (`id`, `name`, `description`, `datetime`, `imageurl`) VALUES
-(1, 'Datsun Truck', 'This truck is meant for delivery of boots ordered from our sites', '2020-10-17 13:01:45', NULL),
-(2, 'Nissan Frontier', 'Mid-Size Pickup Truck', '2020-10-19 13:01:45', NULL),
-(3, 'Volkswagon Trucks', 'for delivering refurbished electronics', '2021-10-28 16:50:55', NULL),
-(4, 'Trucks 001', 'for delivering staples', '2021-10-28 20:56:37', NULL),
-(5, 'Femi Truck', 'test driving and trainingss', '2021-10-28 20:57:02', NULL),
-(6, 'Trucks 003', 'for delivering refurbished electronics', '2021-10-28 20:57:37', NULL),
-(7, 'Trucks 004', 'Truck ern is a multipurpose truck used when any tuck is down', '2021-10-28 20:58:17', NULL),
-(8, 'Trucks 005', 'for movind houseold loads', '2021-10-28 20:59:10', NULL),
-(9, 'Trucks 006', ' is for moving unskilled farm workers  around', '2021-10-28 21:00:03', NULL),
-(10, 'Trucks 007', 'This truck is meant for delivery of boots ordered from our sites', '2021-10-28 21:00:43', NULL),
-(12, 'Trucks 783', 'Truck ern is a multipurpose truck used when any tuck is down', '2021-10-28 22:26:54', NULL),
-(13, 'Trucks jdk', 'Truck hfj is for moving unskilled farm workers  around', '2021-10-28 22:27:15', NULL),
-(14, 'Trucks hjwk', ' is for moving unskilled farm workers  around', '2021-10-28 22:27:34', NULL),
-(15, 'Truck 782', ' is for moving unskilled farm workers  around', '2021-10-28 22:27:52', NULL),
-(16, 'Femi Truck', 'test driving and training', '2021-10-28 22:35:19', NULL),
-(17, 'Oho Truck', 'oho trucks from Ohio', '2021-10-28 22:55:25', NULL),
-(18, 'Oho Truck', 'oho trucks from Ohio', '2021-10-28 23:58:15', NULL),
-(19, 'Oho Truck', 'oho trucks from Ohio', '2021-10-28 23:59:59', 'https://gemmahomecare.com/backend/bejamas/p1_cities_city.jpg');
+(1, 'Datsun Truck', 'This truck is meant for delivery of boots ordered from our sites', '2020-10-17 13:01:45', 'https://gemmahomecare.com/backend/trucks/truck1.jpg'),
+(2, 'Nissan Frontier', 'Mid-Size Pickup Truck', '2020-10-19 13:01:45', 'https://gemmahomecare.com/backend/trucks/truck2.jpg'),
+(3, 'Volkswagon Trucks', 'for delivering refurbished electronics', '2021-10-28 16:50:55', 'https://gemmahomecare.com/backend/trucks/truck3.jpg'),
+(4, 'Trucks 001', 'for delivering staples', '2021-10-28 20:56:37', 'https://gemmahomecare.com/backend/trucks/truck4.jpg'),
+(5, 'Femi Truck', 'test driving and trainingss', '2021-10-28 20:57:02', 'https://gemmahomecare.com/backend/trucks/truck5.jpg'),
+(6, 'Trucks 003', 'for delivering refurbished electronics', '2021-10-28 20:57:37', 'https://gemmahomecare.com/backend/trucks/truck6.jpg'),
+(7, 'Trucks 004', 'Truck ern is a multipurpose truck used when any tuck is down', '2021-10-28 20:58:17', 'https://gemmahomecare.com/backend/trucks/truck7.jpg'),
+(8, 'Trucks 005', 'for movind houseold loads', '2021-10-28 20:59:10', 'https://gemmahomecare.com/backend/trucks/truck8.jpg'),
+(9, 'Trucks 006', ' is for moving unskilled farm workers  around', '2021-10-28 21:00:03', 'https://gemmahomecare.com/backend/trucks/truck9.jpg'),
+(10, 'Trucks 007', 'This truck is meant for delivery of boots ordered from our sites', '2021-10-28 21:00:43', 'https://gemmahomecare.com/backend/trucks/truck10.jpg'),
+(12, 'Trucks 783', 'Truck ern is a multipurpose truck used when any tuck is down', '2021-10-28 22:26:54', 'https://gemmahomecare.com/backend/trucks/truck11.jpg'),
+(13, 'Trucks jdk', 'Truck hfj is for moving unskilled farm workers  around', '2021-10-28 22:27:15', 'https://gemmahomecare.com/backend/trucks/truck12.jpg'),
+(14, 'Trucks hjwk', ' is for moving unskilled farm workers  around', '2021-10-28 22:27:34', 'https://gemmahomecare.com/backend/trucks/truck13.jpg'),
+(15, 'Truck 782', ' is for moving unskilled farm workers  around', '2021-10-28 22:27:52', 'https://gemmahomecare.com/backend/trucks/truck6.jpg'),
+(16, 'Femi Truck', 'test driving and training', '2021-10-28 22:35:19', 'https://gemmahomecare.com/backend/trucks/truck1.jpg'),
+(17, 'Oho Truck', 'oho trucks from Ohio', '2021-10-28 22:55:25', 'https://gemmahomecare.com/backend/trucks/truck11.jpg'),
+(18, 'Oho Truck', 'oho trucks from Ohio', '2021-10-28 23:58:15', 'https://gemmahomecare.com/backend/trucks/truck2.jpg'),
+(19, 'Oho Truck', 'oho trucks from Ohio', '2021-10-28 23:59:59', 'https://gemmahomecare.com/backend/trucks/truck13.jpg'),
+(22, 'Isashi Truck', 'To Ply Isashi Road', '2021-10-29 13:13:29', 'https://gemmahomecare.com/backend/trucks/truck2.jpg'),
+(23, 'Poultry Truck', 'For picking up feeds', '2021-10-29 15:11:31', 'https://gemmahomecare.com/backend/trucks/truck10.jpg'),
+(24, 'Livestock', 'Fore transporting livestocks', '2021-10-29 15:13:39', 'https://gemmahomecare.com/backend/trucks/truck10.jpg'),
+(25, 'Singa Trucks', 'Trucks from Singapore', '2021-10-29 15:31:41', 'https://gemmahomecare.com/backend/trucks/truck8.jpg');
 
 --
 -- Indexes for dumped tables
@@ -110,13 +119,13 @@ ALTER TABLE `trucks`
 -- AUTO_INCREMENT for table `trucklocations`
 --
 ALTER TABLE `trucklocations`
-  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `trucks`
 --
 ALTER TABLE `trucks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
